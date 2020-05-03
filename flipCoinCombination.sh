fe8af706f173bdfdb83a44c04190b1946c129b62
“sandip5” <singhsandeep.singh97@gmail.com>
2020-05-04 04:51:07 +0530

    Storing singlet in dictionary

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,11 +1,27 @@
 #!/bin/bash -x
-
-isHead=0;
-randomCheck=$(( RANDOM%2 ))
-
-if [[ isHead -eq randomCheck ]]
-then
-	echo "Head"
-else
-	echo "Tail"
-fi
+declare -A store
+read -p "Enter the number of times to flip coin : " totalTimes
+isHeads=0;
+isTails=1;
+numberOfHeads=0;
+numberOfTails=0;
+count=$totalTimes
+counter=0;
+while (( $count>0 ))
+do
+	randomCheck=$(( RANDOM%2 ))
+	if [[ $randomCheck -eq $isHeads ]]
+	then
+		((numberOfHeads++))
+		winFace="H";
+	else
+		((numberOfTails++))
+		winFace="T";
+	fi
+	((count--))
+store["$counter"]="$winFace"
+counter=$(( $counter + 1 ))
+done
+echo "${store[@]}"
+percentHead=$( bc <<< "scale-2;($numberOfHeads*100)/$totalTimes" )
+percentTail=$( bc <<< "scale-2;($numberOfTails*100)/$totalTimes" )
