b8185ac81ffd5b75c89043be1ca05c5d8b0c2af5
“sandip5” <singhsandeep.singh97@gmail.com>
2020-05-04 06:36:01 +0530

    Added Doublet UC Three

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -x
 declare -A store
-read -p "Enter the number of times to flip coin : " totalTimes
+read -p "Enter the number of times to flip coin in multiple of 2 : " totalTimes
+
 isHeads=0;
 isTails=1;
 numberOfHeads=0;
 numberOfTails=0;
-j=0;
+arrayCount=0;
 count=$totalTimes
 counter=0;
+
 while (( $count>0 ))
 do
 for (( i=0; i<2; i++ ))
@@ -17,18 +19,55 @@ do
 	then
 		((numberOfHeads++))
 		winHead="H";
-		array[((j++))]="$winHead"
+		array[((arrayCount++))]="$winHead"
 	else
 		((numberOfTails++))
 		winTail="T";
-		array[((j++))]="$winTail"
+		array[((arrayCount++))]="$winTail"
 	fi
 done
-	count=$(( $count - 2 ))
+count=$(( $count - 2 ))
 echo "${array[@]}"
 store["$counter"]="${array[@]:(-2)}"
 counter=$(( $counter + 1 ))
 done
 echo "${store[@]}"
-percentHead=$( bc <<< "scale-2;($numberOfHeads*100)/$totalTimes" )
-percentTail=$( bc <<< "scale-2;($numberOfTails*100)/$totalTimes" )
+comboOne="H H"
+comboTwo="T T"
+comboThree="H T"
+comboFour="T H"
+comboOneCounter=0;
+comboTwoCounter=0;
+comboThreeCounter=0;
+comboFourCounter=0;
+for j in "${store[@]}"
+do
+	if [[ "$j" == "$comboOne" ]]
+	then
+		echo "H H"
+		comboOneCounter=$(( $comboOneCounter + 1))
+	elif [[ "$j" == "$comboTwo" ]]
+	then
+		echo 'T T'
+		comboTwoCounter=$(( $comboTwoCounter + 1 ))
+	elif [[ "$j" == "$comboThree" ]]
+	then
+		echo "H T"
+		comboThreeCounter=$(( $comboThreeCounter + 1 ))
+	elif [[ "$j" == "$comboFour" ]]
+	then
+		echo "T H"
+		comboFourCounter=$(( $comboFourCounter + 1 ))
+	fi
+done
+
+echo $comboOneCounter
+echo $comboTwoCounter
+echo $comboThreeCounter
+echo $comboFourCounter
+
+percentHH=$( bc <<< "scale-2;($comboOneCounter*100)/$totalTimes" )
+percentTT=$( bc <<< "scale-2;($comboTwoCounter*100)/$totalTimes" )
+percentHT=$( bc <<< "scale-2;($comboThreeCounter*100)/$totalTimes" )
+percentTH=$( bc <<< "scale-2;($comboFourCounter*100)/$totalTimes" )
+
